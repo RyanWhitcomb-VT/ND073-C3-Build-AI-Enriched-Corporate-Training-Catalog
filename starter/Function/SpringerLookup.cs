b58e385e92d46399b9e1d509e1f2fe6e9829b914
Ryan Whitcomb <ryantwhitcomb@gmail.com>
2023-08-30 18:03:33 -0400

    Final upload

--- starter/Function/SpringerLookup.cs
+++ starter/Function/SpringerLookup.cs
@@ -23,7 +23,7 @@ namespace Udacity.springerlookupdemo
           This is where you will configure your Spring API Key
           
         */
-        static readonly string apikey = "<insert API credential here>";
+        static readonly string apikey = "73025654ccaf5761ba23d6e6f02912b0";
         static readonly string springerapiendpoint = "http://api.springernature.com/openaccess/json";
 
         #region Class used to deserialize the request
